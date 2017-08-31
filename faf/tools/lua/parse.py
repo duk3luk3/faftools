84cf60692451d561ca4334ac5c96fc83ec45d84e
Lukas Erlacher <l.erlacher@googlemail.com>
2017-08-31 23:47:03 +1000

    Ignore "internal" globals when parsing lua
    When we execute lua scripts to extract the global variables they set, we
    are not interested in the things that already hang around in the globals
    table when the Lua VM is initialized.
    
    Since these things also contain binary that we don't want to run through
    string decoding (resulting in `UnicodeDecodeError`), we're better off
    getting rid of them.

--- faf/tools/lua/parse.py
+++ faf/tools/lua/parse.py
@@ -3,7 +3,6 @@ import pkg_resources
 try:
     import lupa
 
-
     def from_lua(lua_code):
         """
         Use Lupa as a parser by actually running the code
@@ -14,18 +13,23 @@ try:
         fa_functions_path = pkg_resources.resource_filename('static', 'lua/fa_functions.lua')
 
         lua = lupa.LuaRuntime()
+        # we don't care about all of the "internal" globals
+        # especially not the ones that contain binary stuff
+        # resulting in UnicodeDecodeError when we try to unfold them
+        default_globals = dict(lua.globals()).keys()
         with open(fa_functions_path, 'r') as fp:
             lua.execute(fp.read())
 
         lua.execute(lua_code)
 
-        def unfold_table(t, seen=None):
+        def unfold_table(t):
             result = {}
             for k, v in t.items():
-                if not lupa.lua_type(v):  # Already a python type
-                    result[k] = v
-                elif lupa.lua_type(v) == 'table':
-                    result[k] = dict(v)
+                if k not in default_globals:
+                    if not lupa.lua_type(v):  # Already a python type
+                        result[k] = v
+                    elif lupa.lua_type(v) == 'table':
+                        result[k] = dict(v)
             return result
 
         return unfold_table(lua.globals())
